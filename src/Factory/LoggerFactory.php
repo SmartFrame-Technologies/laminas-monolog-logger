570768564e100f28fbcdb599441a97e6de5fc7bf
Tomasz Woźniak <tomasz.wozniak@smartframe.io>
2021-02-23 12:03:44 +0100

    Add abstract factory to create multiple logger services, with shared handlers and processors as option

--- src/Factory/LoggerFactory.php
+++ src/Factory/LoggerFactory.php
@@ -10,33 +10,18 @@ use Psr\Container\ContainerInterface;
 
 final class LoggerFactory implements FactoryInterface
 {
+    use LoggerFactoryTrait;
+
+    protected const CONFIG_KEY = 'logger';
+
     public function __invoke(ContainerInterface $container, $requestedName, array $options = null): Logger
     {
-        $config = $container->get('config')['logger'];
-        $handlers = [];
-        $processors = [];
+        $config = $container->get('config')[self::CONFIG_KEY];
 
-        if (isset($config['handlers'])) {
-            foreach ($config['handlers'] as $handler => $configuration) {
-                if ($container->has($handler)) {
-                    $handlers[] = $container->get($handler);
-                } else {
-                    $handlers[] = new $handler(...array_values($configuration));
-                }
-            }
-        }
-
-        if (isset($config['processors'])) {
-            foreach ($config['processors'] as $processor => $configuration) {
-                if ($container->has($processor)) {
-                    $processors[] = $container->get($processor);
-                } else {
-                    $processors[] = new $processor(...array_values($configuration));
-                }
-            }
-        }
-
-        return new Logger($config['channel_name'], $handlers, $processors);
+        return new Logger($config['channel_name'],
+            $this->processHandlers($container, $config),
+            $this->processProcessors($container, $config)
+        );
     }
 
 }
---
+++ src/Factory/LoggerFactoryTrait.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SmartFrame\Logger\Factory;
+
+
+use Psr\Container\ContainerInterface;
+
+trait LoggerFactoryTrait
+{
+    protected function processHandlers(ContainerInterface $container, array $config): array
+    {
+        $handlers = [];
+
+        if (isset($config['handlers'])) {
+            foreach ($config['handlers'] as $handler => $configuration) {
+                if ($container->has($handler)) {
+                    $handlers[] = $container->get($handler);
+                } else {
+                    $handlers[] = new $handler(...array_values($configuration));
+                }
+            }
+        }
+
+        return $handlers;
+    }
+
+    protected function processProcessors(ContainerInterface $container, array $config): array
+    {
+        $processors = [];
+
+        if (isset($config['processors'])) {
+            foreach ($config['processors'] as $processor => $configuration) {
+                if ($container->has($processor)) {
+                    $processors[] = $container->get($processor);
+                } else {
+                    $processors[] = new $processor(...array_values($configuration));
+                }
+            }
+        }
+
+        return $processors;
+    }
+}
