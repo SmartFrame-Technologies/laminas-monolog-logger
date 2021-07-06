0abc8a191f52544d6858bf092531b547aa3b2598
Tomasz Woźniak <tomasz.wozniak@smartframe.io>
2021-07-06 14:54:16 +0200

    avoid Error exception, and introduce proper Exception class

---
+++ src/HandlerFactory/MissingMethodException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SmartFrame\Logger\HandlerFactory;
+
+use BadMethodCallException;
+
+class MissingMethodException extends BadMethodCallException
+{
+
+}
